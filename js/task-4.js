c33015adaca6a7c90adf39a71d1928ad9bc21822
Veronika Space <veronspaceless@gmail.com>
2025-05-16 15:08:03 +0300

    task4

--- js/task-4.js
+++ js/task-4.js
@@ -1,4 +1,24 @@
 function getShippingCost(country) {
+    let price;
+
+    switch (country) {
+        case "China":
+            price = 100;
+            break;
+        case "Chile":
+            price = 250;
+            break;
+        case "Australia":
+            price = 170;
+            break;
+        case "Jamaica":
+            price = 170;
+            break;
+        default:
+            return `Sorry, there is no delivery to your country`;
+    }
+
+    return `Shipping to ${country} will cost ${price} credits`;
 }
 
 console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
